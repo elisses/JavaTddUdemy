1137e8bb361ce1d63335bc7839a89ed3d5cbb9ff
elisses <fernandalisses@gmail.com>
2018-11-15 16:10:20 +0100

    assertThat/gitignore

--- src/test/java/service/LocacaoServiceTest.java
+++ src/test/java/service/LocacaoServiceTest.java
@@ -3,12 +3,15 @@ package service;
 import Entidade.Filme;
 import Entidade.Locacao;
 import Entidade.Usuario;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 import service.LocacaoService;
 import utils.DataUtils;
 
 import java.util.Date;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class LocacaoServiceTest {
     @Test
@@ -26,9 +29,10 @@ public class LocacaoServiceTest {
 
         //verificacao
 
-        Assert.assertTrue(locacao.getValor() == 5);
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-        Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+        Assert.assertThat(locacao.getValor(), is(CoreMatchers.equalTo(5.0)));
+        Assert.assertThat(locacao.getValor(), is(CoreMatchers.not(6.0)));
+        Assert.assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+        Assert.assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), CoreMatchers.is(true));
 
     }
 }
